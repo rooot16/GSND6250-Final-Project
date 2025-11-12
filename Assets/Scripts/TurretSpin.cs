22beefd93c4e0f295559740bdb420b1af464b7ab
ZiXunY <yanzixun@gmail.com>
2025-11-12 11:41:18 -0500

    Turrets' Detection Area Visualization

--- Assets/Scripts/TurretSpin.cs
+++ Assets/Scripts/TurretSpin.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class TurretSpin : MonoBehaviour
 {
-    // How fast it spins (degrees/second).
+    // How fast this turret spins (in degrees per second).
     public float rotationSpeed = 60f;
 
     void Update()
     {
-        // Get the time passed this frame.
+        // Get the time that passed since the last frame.
         float deltaTime = Time.deltaTime;
 
-        // Calculate the angle to turn.
+        // Figure out how much we need to turn this frame.
         float angleToRotate = rotationSpeed * deltaTime;
 
-        // Spin around our own Y-axis (transform.up).
+        // Spin around the turret's Y-axis (transform.up).
         transform.Rotate(transform.up, angleToRotate, Space.Self);
     }
 }
