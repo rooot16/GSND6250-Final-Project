f94f39decf09bc86d3a9d0444e64d46b86bbd57f
sebastiansalas94 <sebastiansalas94@gmail.com>
2025-12-03 00:10:57 -0500

    Clean lines.

--- Assets/Scripts/IcePackItem.cs
+++ Assets/Scripts/IcePackItem.cs
@@ -11,20 +11,14 @@ public class IcePackItem : MonoBehaviour, Interaction.IInteractable
     {
         MonoBehaviour playerObj = interactor as MonoBehaviour;
 
-
-        // Get the parent's Transform
         Transform parentTransform = transform.parent;
 
-        // Check if a parent exists
         if (parentTransform != null)
         {
-            // Get a reference to the script on the parent (replace ParentScriptType with your actual parent script's class name)
             OpenLid parentScript = parentTransform.GetComponent<OpenLid>();
 
-            // Check if the script exists on the parent
             if (parentScript != null)
             {
-                // Call a public method on the parent script
                 parentScript.ToggleLid();
 
                 await Task.Delay(TimeSpan.FromSeconds(1));
@@ -46,8 +40,6 @@ public class IcePackItem : MonoBehaviour, Interaction.IInteractable
                 Debug.LogWarning("ParentScriptType not found on the parent GameObject.");
             }
         }
-
-
     }
 
     public string GetDescription()
